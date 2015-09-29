2b7f281aceef08a8c1c9d079bdd999e3c3d48719
orvice <orvice@gmail.com>
2015-09-30 01:55:23 +0800

    init views

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controllers;
 
+use Slim\Container;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+
 /**
  * BaseController
  */
@@ -10,5 +14,22 @@ class BaseController
 {
     public function construct__(){
 
+    }
+
+    public function view(){
+        // Create container
+        $container = new Container;
+        // Register component on container
+        $container['view'] = function ($c) {
+            $view = new Twig(BASE_PATH.'/view/', [
+                'cache' => BASE_PATH.'/storage/framework/views'
+            ]);
+            $view->addExtension(new TwigExtension(
+                $c['router'],
+                $c['request']->getUri()
+            ));
+
+            return $view;
+        };
     }
 }
