60b06aaefa0c6e8e99b66268d852e294d7fdcb65
orvice <orvice@gmail.com>
2015-10-08 23:26:02 +0800

    theme support

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -20,12 +20,12 @@ class BaseController
     }
 
     public function smarty(){
+        global $config;
         $smarty=new smarty(); //实例化smarty
-        $smarty->settemplatedir(BASE_PATH.'/views/default/'); //设置模板文件存放目录
+        $smarty->settemplatedir(BASE_PATH.'/views/'.$config['theme'].'/'); //设置模板文件存放目录
         $smarty->setcompiledir(BASE_PATH.'/storage/framework/smarty/compile/'); //设置生成文件存放目录
         $smarty->setcachedir(BASE_PATH.'/storage/framework/smarty/cache/'); //设置缓存文件存放目录
         // add config
-        global $config;
         $smarty->assign('config',$config);
         $this->smarty = $smarty;
         return $smarty;
